28ce5e41ce48ac7433450d886f99f3b96efa23ff
Jean-Luc Duckworth <duckwebdev@gmail.com>
2021-04-06 20:35:33 +0200

    worked on the UI and building the shell code for the grabbing of results

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,15 @@
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('script.js');
-s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+// Load the UI
+fetch(chrome.runtime.getURL('/tool.html'))
+    .then(response => response.text())
+    .then(data => {
+
+        if (typeof $ !== 'function') throw new Error('jQuery or like library missing. will not load tool.');
+        $("body").append(data);
+
+        let s = document.createElement('script');
+        s.src = chrome.runtime.getURL('/script.js');
+        s.onload = function() {
+            this.remove();
+        };
+        (document.head || document.documentElement).appendChild(s);
+    });
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 class ResultExtractor {
 
-    static TOOL_ID = 'ducky-move-tool';
+    static _isRunning = false;
+    static _forceGenerateMap = false; // Did the user forcibly request a map with the current dataset
 
     // Entrypoint into the result extractor tool
     static init() {
@@ -9,23 +10,98 @@ class ResultExtractor {
         let foundMagicText = $("div.ngComp.h2a").text().includes("Property Search Results");
         if (!foundMagicText) return;
 
+        // Grab the extract button
+        let extractButton = $('#ducky-home-tool button._extract');
+
         // Display the extract tool button
-        this._createToolButton();
+        extractButton.show();
+
+        // Bind the button to the extract event
+        extractButton.one('click', this._extract);
+
+        // Make the loading box draggable
+        $('#ducky-home-tool div.loading').draggable();
+
     }
 
-    static _createToolButton() {
-        // Check to see if the button exists already
-        if ($("#" + this.TOOL_ID).length > 0) return;
+    static _resetTool() {
 
-        // // Create the wrapper
-        // let wrapperDiv = $("<div id='ducky-move-tool3'></div>").appendTo("body");
+    }
 
-        // Stylize the wrapper
-        $.get(chrome.extension.getURL('/tool.html'), function(data) {
-            $(data).appendTo('body');
+    /**
+     * Handles pulling the result data from each available page
+     * @private
+     */
+    static async _loadResultData() {
+
+        let hasNextPage = true;
+        let offset = 0;
+
+        while (hasNextPage) {
+
+            // Request result page
+            await $.ajax({
+                type: "GET",
+                url: "/homes/DispatchServlet/Back?Mod=top&OFFSET=" + offset,
+                error: () => {
+                    // Expected due to server-side redirects to http instead of https
+                }
+            });
+
+            //
+
+            // Update the offset
+            offset += 10;
+        }
+
+
+    }
+
+    /**
+     * Starts the process of extracting data from the search results
+     * @private
+     */
+    static async _extract() {
+
+        // Check if there are results
+        if ($("#c4-comp").text().includes("No Matching Listings Found")) {
+            alert('No listings to found');
+            return;
+        }
+
+        // Grab the tool div
+        let toolDiv = $('#ducky-home-tool');
+
+        // Disable the extract button
+        toolDiv.find('button._extract').prop('disabled', true);
+
+        // Grab the skip loading button
+        let loadingBtn = toolDiv.find('button.generate-map');
+
+        // Bind the skip loading button
+        loadingBtn.one('click', () => {
+            // Set our forced flag
+            ResultExtractor._forceGenerateMap = true;
+
+            // Call the map generator
+            ResultExtractor._generateMap();
         });
+
+        // todo: Determine how many pages we are grabbing and update the UI
+
+        // Pull the results
+        await this._loadResultData();
+
+
+        // // Enable the skip loading button
+        // loadingBtn.prop('disabled', false);
+
+
     }
 
+    static _generateMap() {
+
+    }
 }
 
 ResultExtractor.init();
