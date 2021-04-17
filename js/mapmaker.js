95c56a63c60f14bd2fef971164f284e5b91e1146
Jean-Luc Duckworth <duckwebdev@gmail.com>
2021-04-17 23:40:07 +0200

    had to move all the options select to the timeout block

--- js/mapmaker.js
+++ js/mapmaker.js
@@ -3,15 +3,14 @@ function handleRequest(inputData) {
     // Copy the data over
     $('#sourceData').val(inputData.locationData).trigger('change');
 
-    // Update the options
-    $("#region_sel").val(inputData.regionId);
-    $("#lat_sel").val(-1);
-    $("#long_sel").val(-1);
-    $( "#clusterCB" ).prop( "checked", true );
 
     setTimeout(() => {
 
-        // Update the region one more time
+        // Update the options
+        $("#region_sel").val(inputData.regionId);
+        $("#lat_sel").val(-1);
+        $("#long_sel").val(-1);
+        $( "#clusterCB" ).prop( "checked", true );
         $("#region_sel").val(inputData.regionId);
 
         // Make the map
